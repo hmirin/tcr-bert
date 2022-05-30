61c036efaf5ecbd241113a50e3dd6ed95e14fb75
hmirin <hmirin@users.noreply.github.com>
2022-05-31 02:11:22 +0900

    py3.7

--- tcr/featurization.py
+++ tcr/featurization.py
@@ -9,7 +9,7 @@ from functools import lru_cache
 import itertools
 import collections
 from typing import *
-from functools import cached_property
+from backports.cached_property import cached_property
 from math import floor
 
 import numpy as np
